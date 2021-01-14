6ea0a969de7e059c132f10209a3207573bcb9ac0
Bruno Giminiani <giminiani@gmail.com>
2021-01-14 13:18:19 -0300

    feat: save hot posts in memory

---
+++ src/IHotPost.ts
@@ -0,0 +1,8 @@
+export default interface IHotPosts {
+  postTitle: string
+  authorName: string
+  author_name: string
+  creationDate: Date
+  numberOfUps: number
+  numberOfComments: number
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,31 @@
 import express from 'express'
+import axios from 'axios'
+import IHotPost from './IHotPost'
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({ running: true })
+const URI_HOTPOSTS = 'https://api.reddit.com/r/artificial/hot'
+app.post('/', async (request, response) => {
+  const hotPosts: IHotPost[] = []
+  try {
+    const redditResponse = await axios.get(URI_HOTPOSTS)
+    const responseData = redditResponse.data
+    const { children } = responseData.data
+    children.map(child => {
+      const hotPost: IHotPost = {
+        postTitle: child.data.title,
+        authorName: child.data.author_fullname,
+        author_name: child.data.author,
+        creationDate: child.data.created,
+        numberOfUps: child.data.ups,
+        numberOfComments: child.data.num_comments
+      }
+      hotPosts.push(hotPost)
+    })
+    response.json(hotPosts)
+  } catch (error) {
+    console.error(error)
+  }
 })
 
 app.listen('3334', () => console.log('Server is running on port 3334'))
