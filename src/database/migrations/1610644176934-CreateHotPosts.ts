49971e8411ece5b54bf233caf4bdf647625c906e
Bruno Giminiani <giminiani@gmail.com>
2021-01-15 07:45:05 -0300

    chore: add migration

---
+++ src/database/migrations/1610644176934-CreateHotPosts.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateHotPosts1610644176934 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'hotposts',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()'
+          },
+          {
+            name: 'title',
+            type: 'varchar'
+          },
+          {
+            name: 'author_fullname',
+            type: 'varchar'
+          },
+          {
+            name: 'author',
+            type: 'varchar'
+          },
+          {
+            name: 'created',
+            type: 'timestamp with time zone'
+          },
+          {
+            name: 'ups',
+            type: 'integer'
+          },
+          {
+            name: 'comments',
+            type: 'integer'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable('hotposts')
+  }
+}
